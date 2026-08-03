554245fcf89898e1b8e4548032407be3819da02f
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-03 19:58:19 +0700

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
   },
   plugins: [
     react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.ts',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-    },
-  },
-});
