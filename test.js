df448ea6f73364da354bd226b0d375a9c391b3eb
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-21 06:17:24 +0700

    update test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+describe('Payment Flow', () => {
+  it('should complete checkout with COD', () => {
+    cy.visit('/checkout');
+    cy.get('input[name="fullName"]').type('Test User');
+    cy.get('textarea[name="address"]').type('123 Test St, Cypress, CA 90210');
+    cy.contains('เก็บเงินปลายทาง (COD)').click();
+    cy.get('button[type="button"]').should('exist');
+    cy.contains('ยืนยันคำสั่งซื้อ').should('exist');
+  });
+});
