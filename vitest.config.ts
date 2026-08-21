2e77bebecec57f9e5d8e427c6d56bb558379e6c3
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-21 20:59:06 +0700

    update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,18 @@
-import path from 'path';
+import { defineConfig } from "vitest/config";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
+    environment: "jsdom",
     globals: true,
+    setupFiles: "./src/setupTests.ts",
+    exclude: ["node_modules", "cypress/**"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "./src"),
+    },
+  },
+});
